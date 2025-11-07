f4acd8365ed23422ade3c1f205da79f95ed7c05d
MALGIREDDY YOGITHA REDDY <malgireddyy@gmail.com>
2025-11-07 13:54:12 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { ColorModeScript, ChakraProvider } from "@chakra-ui/react";
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import "./index.css";
+import App from "./App";
 import theme from "./theme";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      
-      {/* Wrap App inside BrowserRouter with basename */}
+      {/* Correct basename for GitHub Pages */}
       <BrowserRouter basename="/MYOGITHAREDDY_PORTFOLIO">
         <App />
       </BrowserRouter>
-      
     </ChakraProvider>
   </React.StrictMode>
 );
 
-
 reportWebVitals();
+
